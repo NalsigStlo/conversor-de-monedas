432eeb1e8a83dd284648a18a6adcbb19bc978163
Nicolás Lagos <n.lagos.sepulveda@outlook.com>
2025-05-15 01:26:36 -0400

    creacion clase de conexion a api

--- src/ConexionApi.java
+++ src/ConexionApi.java
@@ -1,2 +1,15 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ConexionApi {
+    private String codigoMoneda;
+
+    public ConexionApi(String codigoMoneda) {
+        this.codigoMoneda = codigoMoneda;
+    }
+
+    public String getCodigoMoneda() {
+        return codigoMoneda;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner inputUsuario = new Scanner(System.in);
 
-        }
+        System.out.println(" Ingrese la moneda que desee verificar: ");
+
+        String codigoMoneda = inputUsuario.nextLine();
+
+        ConexionApi api = new ConexionApi(codigoMoneda);
+
+        HttpClient cliente = HttpClient.newHttpClient();
+        URI direccionApi = URI.create("https://v6.exchangerate-api.com/v6/63de5af194132dbe47c83375/latest/" + api.getCodigoMoneda());
+
+        HttpRequest request = HttpRequest.newBuilder().uri(direccionApi).build();
+
     }
+}
